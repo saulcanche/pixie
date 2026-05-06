dc5deaae9511806c66034553e583c53f01fcacd9
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 13:52:05 -0600

    feat: draw_rectangle implementation

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,12 +1,26 @@
 #include "../include/draw.h"
 
 bool is_valid(Image &img, Point p){ 
-    if(p.y >= img.height || p.y < 0) return false;
-    if(p.x >= img.width || p.x < 0) return false;
+    //point is unsigned so no need to check if x > 0 or y > 0
+    if(p.y >= img.height) return false;
+    if(p.x >= img.width) return false;
     return true;
 }
-void draw_rectangle(Image &img, Point p1, int w, int h, Color col){
+void draw_rectangle(Image &img, Point p1, size_t w, size_t h, Color col){
+    if(!is_valid(img, p1)) return;
+    if(!is_valid(img, {p1.x + w - 1, p1.y + h - 1})) return;
+    for(size_t i = p1.y; i < p1.y+h; i++){
+        for(size_t j = p1.x; j < p1.x + w; j++) set_pixel(img, i, j, col);
+    }
 }
-void draw_circle(Image img, Point c, int radiu, Color col);
-void draw_line_2P(Image img, Point p1, Point p2, Color col);
-void draw_triangle(Image img, Point p1, Point p2, Point p3);
+/*
+void draw_circle(Image img, Point c, int radiu, Color col){
+    return;
+}
+void draw_line_2P(Image img, Point p1, Point p2, Color col){
+    return;
+}
+void draw_triangle(Image img, Point p1, Point p2, Point p3){
+    return;
+} 
+*/
