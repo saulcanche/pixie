634337e8b2742db0fdae8a2939aa0408f6f33872
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 15:36:01 -0600

    chore: updated main to check if the funtion draw_circle is working

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main(){
     Image img = create_image(800, 600);
     fill_image(img, {255, 255, 255});
     draw_rectangle(img, {0, 0}, 799u/2, 599u/2, {255, 0, 0});
+    draw_circle(img, {799u/2, 599u/2}, 100u, {0, 0, 0});
     save_ppm("output.ppm", img);
     return 0;
 }
