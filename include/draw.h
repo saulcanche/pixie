4f388253e46852610c73fd4371bd87f0fe3ab317
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 13:50:10 -0600

    updated header to use size_t as elements since we dont want to work with
    we dont want to work with negative numbers

--- include/draw.h
+++ include/draw.h
@@ -4,11 +4,11 @@
 #include "image.h"
 
 struct Point{
-    int x, y;
+    size_t x, y;
 };
 
-bool is_valid_p(Image &img, Point p);
-void draw_rectangle(Image &img, Point p1, int w, int h, Color col);
+bool is_valid(Image &img, Point p);
+void draw_rectangle(Image &img, Point p1, size_t w, size_t h, Color col);
 void draw_circle(Image &img, Point c, int radiu, Color col);
 void draw_line_2P(Image &img, Point p1, Point p2, Color col);
 void draw_triangle(Image &img, Point p1, Point p2, Point p3);
