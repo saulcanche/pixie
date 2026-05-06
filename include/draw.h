df14bec749b98140d5b8572df02ed284abb645d8
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 15:28:47 -0600

    fix: definition of circle now uses radius unsigned
    We cannot have circles of negative radius

--- include/draw.h
+++ include/draw.h
@@ -9,7 +9,7 @@ struct Point{
 
 bool is_valid(Image &img, Point p);
 void draw_rectangle(Image &img, Point p1, size_t w, size_t h, Color col);
-void draw_circle(Image &img, Point c, int radiu, Color col);
+void draw_circle(Image &img, Point c, size_t radiu, Color col);
 void draw_line_2P(Image &img, Point p1, Point p2, Color col);
 void draw_triangle(Image &img, Point p1, Point p2, Point p3);
 #endif
