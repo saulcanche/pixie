2e7fb92317ad95509633f8ca2670e0089ea4e239
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 16:39:36 -0600

    feat line algorithm implementation

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,5 +1,6 @@
 #include "../include/draw.h"
 
+
 bool is_valid(Image &img, Point p){ 
     //point is unsigned so no need to check if x > 0 or y > 0
     if(p.y >= img.height) return false;
@@ -25,11 +26,31 @@ void draw_circle(Image &img, Point c, size_t radiu, Color col){
         }
     }
 }
-/*
-void draw_line_2P(Image img, Point p1, Point p2, Color col){
+void draw_line(Image &img, Point p1, Point p2, Color col){
+    int dx = abs((int)p2.x - (int)p1.x);
+    int dy = abs((int)p2.y - (int)p1.y);
+    int sx = (p1.x < p2.x) ? 1 : -1;
+    int sy = (p1.y < p2.y) ? 1 : -1;
+    int err = dx - dy;
+    
+    Point p = p1;
+    while(true){
+        if(!is_valid(img, p)) break;
+        set_pixel(img, p.y, p.x, col);
+        
+        if(p.x == p2.x && p.y == p2.y) break;
+        
+        int e2 = 2 * err;
+        if(e2 > -dy){
+            err -= dy;
+            p.x += sx;
+        }
+        if(e2 < dx){
+            err += dx;
+            p.y += sy;
+        }
+    }
+}
+void draw_triangle(Image &img, Point p1, Point p2, Point p3){
     return;
 }
-void draw_triangle(Image img, Point p1, Point p2, Point p3){
-    return;
-} 
-*/
