f2a32643d7aa4d6fecebe22ff78d5ca6784eeb8c
Saul Canche <saulcanche.sc8@gmail.com>
2026-05-06 13:52:53 -0600

    updated main to check new funtion working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "../include/image.h"
+#include "../include/draw.h"
 
 int main(){
     Image img = create_image(800, 600);
     fill_image(img, {255, 255, 255});
+    draw_rectangle(img, {0, 0}, 799u/2, 599u/2, {255, 0, 0});
     save_ppm("output.ppm", img);
     return 0;
 }
